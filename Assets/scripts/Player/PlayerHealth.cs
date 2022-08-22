642a4d03a7f4b99410c267cb456f0bf61f343915
SolovastruAndrei <ssjgohan3.sa@gmail.com>
2022-08-22 14:59:40 +0300

    4

--- Assets/scripts/Player/PlayerHealth.cs
+++ Assets/scripts/Player/PlayerHealth.cs
@@ -1,30 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
+    public TMP_Text deathText;
     public HealthBar healthBar;
+     public  GameoverScript gameover;
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         
-    }
 
-    // Update is called once per frame
-    void Update()
+}
+    private void OnCollisionEnter(Collision collision)
     {
-        if(Input.GetKeyDown(KeyCode.C))
+        if (collision.gameObject.CompareTag("Tinta"))
         {
-            TakeDamage(20);
+            TakeDamage(5);
+         
         }
     }
+
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+        if(currentHealth<0)
+        {
+            Respawn.lives--;
+            Start();
+            deathText.text = "Lives: " + Respawn.lives;
+            if(Respawn.lives<1)
+            {
+                Respawn.lives = 11;
+                gameover.Setup();
+            }
+
+        }
     }
+  
+   
+    
 }
