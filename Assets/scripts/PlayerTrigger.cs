d66717d8f620256f2af19f497f35e02a92d08054
SMG5S <solovastrumihaigabriel@gmail.com>
2022-08-22 12:08:27 +0300

    Made the colliders work in Testare

--- Assets/scripts/PlayerTrigger.cs
+++ Assets/scripts/PlayerTrigger.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerTrigger : MonoBehaviour
 {
     public GameObject player;
-
+    //public Animator anim;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Enemy"))
         {
             player.SetActive(false);
+            //anim.StopPlayback();
+            FindObjectOfType<ChangeSpeedTime>().GetComponent<Animator>().StopPlayback();
             FindObjectOfType<GameManager>().GameOver();
         }
     }
